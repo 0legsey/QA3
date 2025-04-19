d80077fd3dcabfb4650f1b6354ba3961674fe84a
Nikita <nik_naydenko@mail.ru>
2025-04-19 21:58:00 +0700

    Added test for symptoms evaluation that contains temperature and sore throat and removed typo

--- src/Tests/TestTherapist.java
+++ src/Tests/TestTherapist.java
@@ -4,6 +4,10 @@ import Logic.Electronic_Therapist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 class TestTherapist {
     @Test
@@ -19,9 +23,16 @@ class TestTherapist {
     }
 
     @Test
-    void anamnesisCollectionTempAndSoarThroatTest(){
+    void anamnesisCollectionTempAndSoreThroatTest(){
         Electronic_Therapist therapist = new Electronic_Therapist();
         Assertions.assertTrue(therapist.anamnesisCollection().contains("температура") && therapist.anamnesisCollection().contains("боль в горле"));
+    }
+
+    void evaluateSymptomsOfTempAndSoreThroat(){
+        Electronic_Therapist therapist = new Electronic_Therapist();
+        List<String> symptoms = new ArrayList<>(Arrays.asList("температура", "боль в горле"));
+        String expected_diagnosys = "Возможный диагноз - ангина/ОРВИ. Необходима консультация врача";
+        Assertions.assertEquals(expected_diagnosys, therapist.evaluateSymptoms(symptoms));
     }
 
 }
