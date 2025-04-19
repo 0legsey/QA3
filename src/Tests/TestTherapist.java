a1bec1146b0e1679be4f2df48b7abd489e8663b2
Nikita <nik_naydenko@mail.ru>
2025-04-19 21:10:22 +0700

    Added test for anamnesis that contains high temperature

--- src/Tests/TestTherapist.java
+++ src/Tests/TestTherapist.java
@@ -4,10 +4,18 @@ import Logic.Electronic_Therapist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+
 class TestTherapist {
     @Test
     void therapistClassCreationTest(){
         Electronic_Therapist therapist = new Electronic_Therapist();
         Assertions.assertNotNull(therapist);
     }
+
+    @Test
+    void anamnesisCollectionTemperatureTest(){
+        Electronic_Therapist therapist = new Electronic_Therapist();
+        Assertions.assertTrue(anamnesisCollection().contains("температура"));
+    }
+
 }
