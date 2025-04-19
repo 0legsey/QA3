72cf1332cbc286774f108a5465664468e54b396e
Nikita <nik_naydenko@mail.ru>
2025-04-19 21:21:26 +0700

    Added trivial implementation of anamnesisCollection method and fixed previosly missing object name in assertTrue method

--- src/Logic/Electronic_Therapist.java
+++ src/Logic/Electronic_Therapist.java
@@ -1,5 +1,15 @@
 package Logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Electronic_Therapist {
     public Electronic_Therapist(){}
+
+    public List<String> anamnesisCollection(){
+        List<String> symptoms = new ArrayList<>();
+        symptoms.add("температура");
+        return symptoms;
+    }
 }
